605891a0a173217a1999fd6aa182457273fb7c80
Minami Takayama <m.inam.ina03037@gmail.com>
2025-04-07 23:49:09 +0900

    react lecture for today

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,61 @@ import Sidebar from './components/Sidebar/Sidebar';
 
 
 const App =()=>{
+  const cafeDatas = [
+    {
+        "picture": "https://laptopfriendly.co/images/places/tokyo/ddsk-saigon-kitchen/ddsk-saigon-kitchen--tokyo.jpg",
+        "title": "DDSK SAIGON KITCHEN",
+        "address": "ss, 135-0061, Tokyo",
+        "criteria": []
+    },
+    {
+        "picture": "https://laptopfriendly.co/images/places/tokyo/common/common--tokyo.jpg",
+        "title": "COMMON",
+        "address": "106-0032, Tokyo",
+        "criteria": [
+            "Stable Wi-Fi",
+            "Power sockets",
+            "Quiet",
+            "Coffee",
+            "Food"
+        ]
+    },
+    {
+        "picture": "https://laptopfriendly.co/images/places/tokyo/brooklyn-parlor-shinjuku/brooklyn-parlor-shinjuku%20shinjuku-city%20brooklyn-parlor-shinjuku-shinjuku-city%20.jpg",
+        "title": "BROOKLYN PARLOR SHINJUKU",
+        "address": "Shinjuku Marui Annex B1F, 3-1-26 Shinjuku, Shinjuku-ku, Tokyo, 160-0022, Tokyo",
+        "criteria": [
+            "Stable Wi-Fi",
+            "Power sockets",
+            "Quiet",
+            "Coffee",
+            "Food"
+        ]
+    }
+  ];
+
+  const getCafes = () =>{
+    console.log("Getting cafes...");
+    const url = "https://matcha-and-keyboard-f549965e60e7.herokuapp.com/api/v1/cafes";
+    fetch(url)
+      .then(response =>response.json())
+      .then((data) => {
+        console.log(data);
+        cafeDatas = data;
+      });
+  };
+
+
   return(
     <div className="app-frame">
       <Navbar/>
+      {/*to call function: onClick={nameOfFunction} */}
+      <button onClick={getCafes}>
+        Get All Cafes!
+      </button>
       <div className="app-body">
         <Sidebar/>
-        <CafeList/>
+        <CafeList cafeDatas={cafeDatas}/>
       </div>
     </div>
   );
--- src/components/CafeList/CafeList.jsx
+++ src/components/CafeList/CafeList.jsx
@@ -3,39 +3,7 @@ import Cafe from "../Cafe/Cafe";
 
 // rsc(ReactStatelessComponent) + Tab ===> build the skeleton
 
-const CafeList = () => {
-  const cafeDatas = [
-    {
-        "picture": "https://laptopfriendly.co/images/places/tokyo/ddsk-saigon-kitchen/ddsk-saigon-kitchen--tokyo.jpg",
-        "title": "DDSK SAIGON KITCHEN",
-        "address": "ss, 135-0061, Tokyo",
-        "criteria": []
-    },
-    {
-        "picture": "https://laptopfriendly.co/images/places/tokyo/common/common--tokyo.jpg",
-        "title": "COMMON",
-        "address": "106-0032, Tokyo",
-        "criteria": [
-            "Stable Wi-Fi",
-            "Power sockets",
-            "Quiet",
-            "Coffee",
-            "Food"
-        ]
-    },
-    {
-        "picture": "https://laptopfriendly.co/images/places/tokyo/brooklyn-parlor-shinjuku/brooklyn-parlor-shinjuku%20shinjuku-city%20brooklyn-parlor-shinjuku-shinjuku-city%20.jpg",
-        "title": "BROOKLYN PARLOR SHINJUKU",
-        "address": "Shinjuku Marui Annex B1F, 3-1-26 Shinjuku, Shinjuku-ku, Tokyo, 160-0022, Tokyo",
-        "criteria": [
-            "Stable Wi-Fi",
-            "Power sockets",
-            "Quiet",
-            "Coffee",
-            "Food"
-        ]
-    }
-  ];
+const CafeList = ({cafeDatas}) => {
 
   return (
     <div className="cafe-list">
